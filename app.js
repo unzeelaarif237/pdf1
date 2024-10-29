b601d9928006c0620c877af500ac53bdb1b21d98
unzeelaarif237 <unzeelaarifarif@gmail.com>
2024-10-29 18:15:26 +0500

    1to2

---
+++ app.js
@@ -0,0 +1,41 @@
+// 1
+var username = "";
+
+// 2
+var myName = "Unzeela";
+// 3
+var message = "Hello World";
+alert(message);
+// 4
+var Name = "Minha";
+alert(Name);
+var age = "17 years old";
+alert(age);
+var degree = "Certified  Web Mobile Application Development";
+alert(degree);
+// 5
+var food = "Biryani";
+var result= "";
+for (let i = food.length; i > 0; i--) {
+    result += food.slice(0, i) + "\n"; 
+}
+alert(result)
+// 6
+var email = "";
+email= "unzeelaunzeela3@gmail.com";
+alert("MY email adress is " + email);
+
+// 7
+var book= "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// 8
+var html = "Yah! I can write HTML content through JavaScript";
+document.write(html);
+
+document.write('<br>');
+
+// 9
+var same = "showing same work in alert box and in html ";
+alert(same);
+document.write(same);
